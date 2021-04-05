24732959a3d8995e5a461966ff41cfb09f37d431
mxr <eetu.kivelae@gmail.com>
2021-04-05 20:05:44 +0300

    Moved Cell struct to cell_grid module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use sfml::{
     graphics::{
-        Color, Drawable, RectangleShape, RenderStates, RenderTarget, RenderWindow,
-        Shape, Transformable, Font
+        RenderWindow, Font
     },
     window::{Event, Key, Style},
 };
@@ -9,43 +8,8 @@ use sfml::{
 mod app_state;
 use app_state::AppState;
 
-pub enum CellState {
-    Alive,
-    Dead,
-}
-
-pub struct Cell {
-    state: CellState,
-    pos: (f32, f32),
-}
-
-impl Cell {
-    pub fn new(state: CellState, pos: (f32, f32)) -> Cell {
-        Self {
-            state,
-            pos,
-        }
-    }
-}
-
-impl <'s> Drawable for Cell {
-    fn draw<'a: 'shader, 'texture, 'shader, 'shader_texture>(
-        &'a self,
-        render_target: &mut dyn RenderTarget,
-        _: &RenderStates<'texture, 'shader, 'shader_texture>,
-    ) {
-        use CellState::*;
-        let mut square = RectangleShape::with_size((10., 10.).into());
-        let opacity: u8 = match self.state {
-            Alive => 255,
-            Dead => 0,
-        };
-        square.set_fill_color(Color::WHITE);
-        square.set_position(self.pos);
-        square.set_fill_color(Color::rgba(255, 255, 255, opacity));
-        render_target.draw(&square)
-    }
-}
+mod cell_grid;
+use cell_grid::{Cell, CellState};
 
 fn main() {
     let mut window = RenderWindow::new(
